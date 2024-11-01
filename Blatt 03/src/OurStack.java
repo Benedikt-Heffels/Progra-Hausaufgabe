773da5baa1a51b743f96d57165ecac504978d07e
Benedikt-Heffels <benedikt.heffels@gmail.com>
2024-11-01 15:19:54 +0100

    added Blatt03 Aufgabe 3 (OurStack) Kompiliert unter javac

--- Blatt 03/src/OurStack.java
+++ Blatt 03/src/OurStack.java
@@ -32,9 +32,9 @@ public class OurStack {
   }
 
   public void push() {
-    if(currentSize < stack.length){ //currentSize und stack.length sind beide +1 verschoben zur realen Position. <, da min. ein Platz noch freis ein muss.
+    if(currentSize < stack.length){ //currentSize und stack.length sind beide +1 verschoben zur realen Position. <, da mindestens ein Platz noch frei sein muss.
       stack[currentSize] = SimpleIO.getString("Geben Sie ein zu speicherndes Element ein: ");
-      //Da Stack immer von links nach rechts aufgefüllt wird und currenSize ein Element weiter liegt als das letzte Elemenet, lässt sich do die push-Methode implementieren
+      //Da Stack immer von links nach rechts aufgefüllt wird und currentSize ein Element weiter liegt als das letzte Element, lässt sich do die push-Methode implementieren
       currentSize = currentSize + 1;
     }
     else {
@@ -59,21 +59,21 @@ public class OurStack {
     if (size >= currentSize) { //Wenn der neue Stack größer ist als der alte: Alle Elemente können übernommen werden
       if (currentSize != 0) {
         String[] help = new String[currentSize]; //Es wird ein Hilfsstack erzeugt
-        for (int i = 0; i < currentSize; i++) { //Kopieren, um Inhalt bei redefinition des Hauptsstacks nicht zu verlieren
+        for (int i = 0; i < currentSize; i++) { //Kopieren, um Inhalt bei redefinition des Hauptstacks nicht zu verlieren
           help[i] = stack[i];
         }
         stack = new String[size]; //Setzen der neuen Größe
         for (int i = 0; i < help.length; i++) { //Einsetzen des neuen Stacks
           stack[i] = help[i];
         }
-        //currentSize behält den Wert, den es zuvor hatte, da die Anzahl der Elmenete im Stack nicht verändert werden
+        //currentSize behält den Wert, den es zuvor hatte, da die Anzahl der Elemente im Stack nicht verändert werden
       }
       else { //Falls aktuelle Länge (currentSize) == 0, so muss der alte Stack nicht kopiert werden
         stack = new String[size];
         //currentSize bleibt in diesem Falle 0, wie es auch bereits zuvor war.
       }
     }
-    else { //Wenn der alte Stack größer war als der neue: Nur so viele Elemente, wie in den Stack rein passen, werden kopiert
+    else { //Wenn der alte Stack größer war als der neue: Nur so viele Elemente, wie in den Stack hineinpassen, werden kopiert
       String[] help = new String[size]; //Es wird ein Hilfsstack erzeugt
       for (int i = 0; i < size; i++) { //Kopieren, um zwischenzuspeichern. Begrenzung führt dazu, dass zu viele Elemente verloren gehen.
         help[i] = stack[i];
