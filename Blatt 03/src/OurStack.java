205d19877794d777f6880175614ceb119dca5715
Benedikt-Heffels <benedikt.heffels@gmail.com>
2024-11-03 15:40:53 +0100

    Entfernung der Sonderzeichen

--- Blatt 03/src/OurStack.java
+++ Blatt 03/src/OurStack.java
@@ -33,11 +33,11 @@ public class OurStack {
 
   public void push() {
     if (stack == null) {
-      SimpleIO.output("Bitte erst Länge des Stacks definieren.");
+      SimpleIO.output("Bitte erst Laenge des Stacks definieren.");
     }
     else if(currentSize < stack.length){ //currentSize und stack.length sind beide +1 verschoben zur realen Position. <, da mindestens ein Platz noch frei sein muss.
       stack[currentSize] = SimpleIO.getString("Geben Sie ein zu speicherndes Element ein: ");
-      //Da Stack immer von links nach rechts aufgefüllt wird und currentSize ein Element weiter liegt als das letzte Element, lässt sich do die push-Methode implementieren
+      //Da Stack immer von links nach rechts aufgefuellt wird und currentSize ein Element weiter liegt als das letzte Element, laesst sich do die push-Methode implementieren
       currentSize = currentSize + 1;
     }
     else {
@@ -47,7 +47,7 @@ public class OurStack {
 
   public void pop() {
     if (currentSize > 0) { //Stack nicht leer
-      stack[currentSize - 1] = ""; //Wird auf leeren String zurückgesetzt
+      stack[currentSize - 1] = ""; //Wird auf leeren String zurueckgesetzt
       currentSize = currentSize - 1;
     }
     //Befindet sich kein Element im Stack, dann soll nichts passiert
@@ -55,35 +55,33 @@ public class OurStack {
 
   public void clear() {
     int laengeStack = stack.length;
-    stack = new String[laengeStack-1];
+    stack = new String[laengeStack];
     currentSize = 0;
   }
 
   public void setSize(int size) {
-    if (size > currentSize) { //Wenn der neue Stack größer ist als der alte: Alle Elemente können übernommen werden
+    if(size == currentSize) return; //Wenn size == currentSize, muss am Stack nichts veraendert werden. Dann kann das Programm abbrechen.
+    if (size > currentSize) { //Wenn der neue Stack groesser ist als der alte: Alle Elemente koennen uebernommen werden
       if (currentSize != 0) {
         String[] help = stack;
-        stack = new String[size]; //Setzen der neuen Größe
+        stack = new String[size]; //Setzen der neuen Groesse
         for (int i = 0; i < help.length; i++) { //Einsetzen des neuen Stacks
           stack[i] = help[i];
         }
-        //currentSize behält den Wert, den es zuvor hatte, da die Anzahl der Elemente im Stack nicht verändert werden
+        //currentSize behaelt den Wert, den es zuvor hatte, da die Anzahl der Elemente im Stack nicht veraendert werden
       }
-      else { //Falls aktuelle Länge (currentSize) == 0, so muss der alte Stack nicht kopiert werden
+      else { //Falls aktuelle Laenge (currentSize) == 0, so muss der alte Stack nicht kopiert werden
         stack = new String[size];
         //currentSize bleibt in diesem Falle 0, wie es auch bereits zuvor war.
       }
     }
-    else if (size == currentSize){ //Wenn size == currentSize, muss am Stack nichts verändert werden. Dann kann das Programm abbrechen.
-        return;
-    }
-    else { //Wenn der alte Stack größer war als der neue: Nur so viele Elemente, wie in den Stack hineinpassen, werden kopiert
+    else { //Wenn der alte Stack groesser war als der neue: Nur so viele Elemente, wie in den Stack hineinpassen, werden kopiert
       String[] help = new String[size]; //Es wird ein Hilfsstack erzeugt
-      for (int i = 0; i < size; i++) { //Kopieren, um zwischenzuspeichern. Begrenzung führt dazu, dass zu viele Elemente verloren gehen.
+      for (int i = 0; i < size; i++) { //Kopieren, um zwischenzuspeichern. Begrenzung fuehrt dazu, dass zu viele Elemente verloren gehen.
         help[i] = stack[i];
       }
-      stack = new String[size]; //Setzen der neuen Größe
-      currentSize = size; //Redefinition der Stack-Größe - hier nötig, da Stack geschrumpft
+      stack = new String[size]; //Setzen der neuen Groesse
+      currentSize = size; //Redefinition der Stack-Groesse - hier noetig, da Stack geschrumpft
       for (int i = 0; i < help.length; i++) { //Einsetzen des neuen Stacks
         stack[i] = help[i];
       }
@@ -92,7 +90,7 @@ public class OurStack {
 
   public void print() {
     String druck = "Stack: "; //Ausgabe in String erstellen
-    for (int i = 0; i < currentSize; i++) { //Für die print-Ausgabe wird der gesamte Stack per for-Schleife durchlaufen
+    for (int i = 0; i < currentSize; i++) { //Fuer die print-Ausgabe wird der gesamte Stack per for-Schleife durchlaufen
       if (i > 0) {
         druck = druck.concat(", ");
       }
